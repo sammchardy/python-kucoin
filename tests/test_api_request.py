ce35efca44dc1948b6b76ab848c2ba0806434b72
Sam McHardy <hello@sammchardy.net>
2018-01-14 21:25:54 +1100

    Catch success false type error responses

--- tests/test_api_request.py
+++ tests/test_api_request.py
@@ -48,3 +48,24 @@ def test_api_error_exception():
             }
             m.get('https://api.kucoin.com/v1/open/chart/symbol?symbol=KCS-BTC', json=json_obj, status_code=400)
             client.get_symbol_tv('KCS-BTC')
+
+
+def test_api_200_exception():
+    """Test API response Exception"""
+
+    with pytest.raises(KucoinAPIException):
+        with requests_mock.mock() as m:
+            json_obj = {
+                "success": False,
+                "code": "NO_BALANCE",
+                "msg": "Insufficient balance: BTC",
+                "timestamp": 1515925287831,
+                "data": {
+                    "coinType": "BTC",
+                    "expect": 0.00465456,
+                    "actual": 2.6447E-4
+                }
+            }
+
+            m.get('https://api.kucoin.com/v1/user/info', json=json_obj, status_code=200)
+            client.get_user()
